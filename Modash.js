2007fcdff3591e149a2cb2c8d8b12b1a8b5d9eb9
Adler Melgaço Ferreira <adlermf07@gmail.com>
2021-01-07 17:34:08 -0300

    truncate function tested

--- Modash.js
+++ Modash.js
@@ -1,6 +1,6 @@
 // We write the Modash library in this file in the Unit Testing chapter
 function truncate(string, length) {
-	if (string.length > lenght) {
+	if (string.length > length) {
 		return string.slice(0, length) + '...';
 	} else {
 		return string;
