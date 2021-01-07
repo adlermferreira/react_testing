c81deaab14330b1b86e4c5c809a25d5e62c78f68
Adler Melgaço Ferreira <adlermf07@gmail.com>
2021-01-07 18:00:19 -0300

    truncate test refactored

--- Modash.test.js
+++ Modash.test.js
@@ -1,13 +1,21 @@
 import Modash from './Modash';
 
-const string = 'there was one catch, and that was CATCH-22';
-const actual = Modash.truncate(string, 19);
-const expected = 'there was one catch...';
-
-if(actual !== expected) {
-    console.log(
-        `[FAIL] Expected \`truncate()\` to return '${expected}', got '${actual}'` 
-    );
-} else {
-    console.log('[PASS] `truncate()`.');
+function assertEqual(description, actual, expected) {
+    if (actual === expected) {
+        console.log(`[PASS] ${description}`);
+    } else {
+      console.log(`[FAIL] ${description}`);
+      console.log(`\tactual: '${actual}'`);
+      console.log(`\texpected: '${expected}'`);
+    }
 }
+
+let actual;
+let expected;
+let string;
+
+string = 'there was one catch, and that was CATCH-22';
+actual = Modash.truncate(string, 19);
+expected = 'there was one catch...';
+
+assertEqual('`truncate()`: truncates a string', actual, expected);
