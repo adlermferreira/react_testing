4f0e4270ffcaa828b4b810f8ec8bfe27d15cd019
Adler Melgaço Ferreira <adlermf07@gmail.com>
2021-01-07 18:10:23 -0300

    more assertions added

--- Modash.test.js
+++ Modash.test.js
@@ -17,5 +17,22 @@ let string;
 string = 'there was one catch, and that was CATCH-22';
 actual = Modash.truncate(string, 19);
 expected = 'there was one catch...';
-
 assertEqual('`truncate()`: truncates a string', actual, expected);
+
+actual = Modash.truncate(string, string.length);
+expected = string;
+assertEqual('`truncate()`: no-ops if <= length', actual, expected);
+
+actual = Modash.capitalize(string);
+expected = 'There was one catch, and that was catch-22';
+assertEqual('`capitalize()`: capitalizes the string', actual, expected);
+
+string = 'customer responded at';
+actual = Modash.camelCase(string);
+expected = 'customerRespondedAt';
+assertEqual('`camelCase()`: string with spaces', actual, expected);
+
+string = 'customer_responded_at';
+actual = Modash.camelCase(string);
+expected = 'customerRespondedAt';
+assertEqual('`camelCase()`: string with underscores', actual, expected);
